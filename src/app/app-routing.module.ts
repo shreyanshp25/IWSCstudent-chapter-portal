379f1028ce37fde3d72d1ae65ab501bdaebf8e1b
shreyanshp25 <shreyansh2511@gmail.com>
2021-01-15 23:17:14 +0530

    initial change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutUsComponent } from './homeSec/about-us/about-us.component';
+import { ScComponent } from './homeSec/sc/sc.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component:AboutUsComponent},
+  {path: 'studentchapters', component: ScComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [AboutUsComponent, ScComponent];
--- src/app/homeSec/header/header.component.ts
+++ src/app/homeSec/header/header.component.ts
@@ -10,9 +10,7 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-  //Form validation for NEW SC---
-  
+  }  
   //Random data fillers-----
   about = `Inception Wave is an organisation that combines the entrepreneurial innovation driven mindset of an emerging and vibrant youth community with the colossal upsides and tremendous convenience that comes with effective connection and collaboration. Our vision is to effectively enhance professional culture by enabling our users to find the people and communities there personal and professional interests align with. This is envisioned as a process that will compartmentalize and segregate online spaces on our services via parameters such as geographical location, interests, experiences and potential prospects. The idea is to present and furnish a platform where it is the users who decide their preferences and are able to come across an extensive community of like-minded people that share common aspirations and a more-or less unified outlook. Being a student-led and student-run enterprise, the key feature here is flexibility, adaptability, and dynamic feedback response`
   studentChapterName = ["SRMIST Chennai", "VIT Vellore", "MIT Manglore", "IIT Madras"];
