558bbe12780eccf3bf074fd8008f47615a2e365d
shreyanshp25 <shreyansh2511@gmail.com>
2021-01-16 01:06:46 +0530

    15-1 work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './homeSec/header/header.component';
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './homeSec/footer/footer.component';
+import { AboutUsComponent } from './homeSec/about-us/about-us.component';
+import { ScComponent } from './homeSec/sc/sc.component';
 import { NewScFormComponent } from './homeSec/new-sc-form/new-sc-form.component';
 
 @NgModule({
@@ -15,8 +17,11 @@ import { NewScFormComponent } from './homeSec/new-sc-form/new-sc-form.component'
     HeaderComponent,
     routingComponents,
     FooterComponent,
+    AboutUsComponent,
+    ScComponent,
+    NewScFormComponent
   ],
-  entryComponents : [NewScFormComponent],
+  //entryComponents : [],
   imports: [
     BrowserModule,
     AppRoutingModule,
